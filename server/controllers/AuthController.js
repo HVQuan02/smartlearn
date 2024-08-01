6addbd4a815908197e039796fdb002488e2f9f0e
HVQuan02 <hovquan02@gmail.com>
2024-08-01 09:46:44 +0700

    Add sign in, add hash password

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 class AuthController {
@@ -9,8 +10,33 @@ class AuthController {
   // [POST] /auth/sign-up
   async signUp(req, res) {
     try {
+      const saltRounds = 11
+      req.body.password = await bcrypt.hash(req.body.password, saltRounds)
       await User.create(req.body)
-      res.send('User created successfully!') // debug
+      res.send('User created successfully!')
+    }
+    catch(err) {
+      res.status(500).send(err)
+    }
+  }
+
+  // [POST] /auth/sign-in
+  async signIn(req, res) {
+    try {
+      const { email, password } = req.body
+      const user = await User.findOne({ email })
+      if (user) {
+        const isValidPw = await bcrypt.compare(password, user.password)
+        if (isValidPw) {
+          res.send('User signed in successfully!')
+        }
+        else {
+          res.send('Incorrect password!')
+        }
+      }
+      else {
+        res.send('User does not exist!')
+      }
     }
     catch(err) {
       res.status(500).send(err)
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,5 +5,6 @@ authRouter = express.Router()
 
 authRouter.get('/', AuthController.index)
 authRouter.post('/sign-up', AuthController.signUp)
+authRouter.post('/sign-in', AuthController.signIn)
 
 module.exports = authRouter
